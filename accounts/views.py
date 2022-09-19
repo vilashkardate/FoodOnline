1185fd07fa7989dff524d0200b88fe8b463b8036
Vilash Kardate <vilash@Vilashs-MacBook-Air.local>
2022-09-19 17:05:42 +0530

    home page , templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from http.client import HTTPResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
 
 # Create your views here.
+def registerUser(request):
+    if(request.method=='POST'):
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # Create the user using the form
+            # password=form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.role=User.CUSTOMER
+            # user.set_password(password)
+            # user.save()
+
+
+            # Create the user using create user method 
+            first_name  = form.cleaned_data['first_name']
+            last_name   = form.cleaned_data['last_name']
+            username    = form.cleaned_data['username']
+            email       = form.cleaned_data['email']
+            password    = form.cleaned_data['password']
+            user   = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,password=password,email=email)
+            user.role=User.CUSTOMER
+            user.save()
+            messages.success(request,"Your account has been register sucessfully  ")
+            return redirect('registerUser')
+        else:
+            print("form is not valid")
+            print(form.errors)
+    else:
+        print("Inside else")
+        form = UserForm()
+    context={
+        'form':form,
+    }
+    return render(request,'accounts/registerUser.html',context)
